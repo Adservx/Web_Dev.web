3a880a321a14de334426ceb8f1aa298facd4331c
Adservx <imserv67@gmail.com>
2024-05-29 14:22:35 +0545

    Initial commit

---
+++ xxx.js
@@ -0,0 +1,10 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+function addTwoNumbers(num1, num2) 
+{
+    return num1 + num2;
+}
+
+let sum = addTwoNumbers(num1, num);
+alert("The sum of the two numbers is: " + sum);
