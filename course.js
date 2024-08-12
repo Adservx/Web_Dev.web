25ce56caf89168e6eb1998773e7222246d6f201f
Adservx <imserv67@gmail.com>
2024-08-13 03:13:25 +0545

    v
    ok in more about course

--- course.js
+++ course.js
@@ -1,11 +1,6 @@
-function redirectToAnotherPage() {
-    window.location.href = 'http://ctevt.org.np/uploads/docs/2021-01-24_TSLC%20in%20Electrical%20Engineering_Post%20SLC,%20%20Revised%202016.pdf';
+function downloadPDF(filename) {
+    const link = document.createElement('a');
+    link.href = filename;
+    link.download = filename;
+    link.click();
 }
-
-function redirectToAnotherPagex() {
-    window.location.href = 'http://ctevt.org.np/uploads/docs/2024-05-09_Diploma%20in%20Electrical%20Engineering_revised_2022.pdf';
-}
-function redirectToAnotherPagez() {
-    window.location.href = 'http://ctevt.org.np/uploads/docs/2023-03-30_diploma_in_engineering_first_year_all_2021.pdf';
-}
-
