2dfb3e089f7ddd087fd1c637bbed3c66ac699a07
Adservx <imserv67@gmail.com>
2024-10-07 12:09:23 +0545

    yui

--- idx.js
+++ idx.js
@@ -1,1 +1,4 @@
-alert("This website is still developing.... it may took several month.Click ok to continue for sample of it..!!");
+alert("⚠️WARNING:This website is still under development!⚠️"
+);
+alert("It may took several weeks.Click ok to continue for sample of it..!!")
+alert("Domain is powered by vercel.app");
